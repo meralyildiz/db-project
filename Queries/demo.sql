8c3de32a3ab3f4e564a691436bc6f84503bfe843
Meral <meral.yildiz@hotmail.com>
2022-11-23 21:47:36 +0100

    commit started

---
+++ Queries/demo.sql
@@ -0,0 +1,16 @@
+select * from EMPLOYEES;
+--reads all column from employees table
+
+select * from DEPARTMENTS;
+--reads all column from departments table
+
+select FIRST_NAME from EMPLOYEES;
+-- get only first name from employees table
+
+select CITY from LOCATIONS ;
+--display city names
+
+select FIRST_NAME, LAST_NAME, SALARY from EMPLOYEES ;
+--get me first_name, last_name and salary
+--to retrieve different columns we need to write column names and comma (,)
+
